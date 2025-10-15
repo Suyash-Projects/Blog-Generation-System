10127f027e89475ea06b2e46cfb9c1501969cb72
suyash1574 <zinjurke77h@gmail.com>
2025-10-15 18:51:22 +0530

    port updated

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def save_blog():
         return jsonify({'error': f'An error occurred while saving the blog: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
